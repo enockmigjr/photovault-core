087e6bc35cae130e3738baba8ed90bfc8718f27c
Enock Junior Mig <avelyrio229@gmail.com>
2026-07-08 09:46:23 +0100

    feat(identity): move auth handlers out of core

--- photovault-core.php
+++ photovault-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: PhotoVault Core
  * Description: Core application layer for PhotoVault media, access rules, REST endpoints, roles, and statistics.
- * Version: 0.1.4
+ * Version: 0.1.5
  * Author: PhotoVault
  * Text Domain: photovault
  */
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'PHOTOVAULT_CORE_VERSION', '0.1.4' );
+define( 'PHOTOVAULT_CORE_VERSION', '0.1.5' );
 define( 'PHOTOVAULT_CORE_DIR', plugin_dir_path( __FILE__ ) );
 define( 'PHOTOVAULT_CORE_URI', plugin_dir_url( __FILE__ ) );
 
@@ -19,7 +19,6 @@ $photovault_core_includes = array(
 	'inc/roles.php',
 	'inc/post-types.php',
 	'inc/taxonomies.php',
-	'inc/auth-handlers.php',
 	'inc/media-handlers.php',
 	'inc/ajax-filters.php',
 	'inc/helpers.php',
